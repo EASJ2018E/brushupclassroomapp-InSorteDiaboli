848ab18863de88dba04302de07d0ad8ee1e61c04
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2018-09-05 13:39:23 +0200

    Added class Studerende

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Studerende
+    {
+        public string Navn;
+        public int Fødselsmåned;
+        public int Fødselsdag;
+    }
+}
