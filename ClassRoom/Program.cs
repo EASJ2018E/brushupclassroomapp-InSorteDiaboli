b46957265f6dc2a583717244b7f8032f70d907c8
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2018-09-06 10:32:51 +0200

    Added new values to KlasseNavn, SemesterStart and Studerende in Main method

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,6 +10,11 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            KlasseRum klasseRum = new KlasseRum();
+            klasseRum.Klassenavn = "3B";
+            klasseRum.SemesterStart = new DateTime(4/9/2018);
+            klasseRum.Klasseliste = new List<Studerende>{new Studerende("Frederik", 2,2), new Studerende("Louis", 3, 3), new Studerende("Vagner", 5, 6)};
+
         }
     }
 }
